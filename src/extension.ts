30f1fee17cd88e6d2663687e3ec64454ba6c5d6b
Andrii Lobanov <anloba@google.com>
2026-01-15 15:40:50 +0000

    extension cleanup, as entrpypoint
    Change-Id: Ic58bba6f971c93d1d5639ead8eefc2df63f8ff17

--- src/extension.ts
+++ src/extension.ts
@@ -34,8 +34,6 @@ import { ProjectsClient } from "./workbench/projects-client";
  */
 export async function activate(context: vscode.ExtensionContext) {
   const jupyter = await getJupyterApi(vscode);
-  const uriHandler = new ExtensionUriHandler(vscode);
-  const uriHandlerRegistration = vscode.window.registerUriHandler(uriHandler);
   const authClient = new OAuth2Client(
     CONFIG.ClientId,
     CONFIG.ClientNotSoSecret,
@@ -80,55 +78,13 @@ export async function activate(context: vscode.ExtensionContext) {
     jupyter,
   );
 
-  const keepServersAlive = new ServerKeepAliveController(
-    vscode,
-    colabClient,
-    assignmentManager,
-  );
-  const consumptionMonitor = watchConsumption(colabClient);
-  // Sending server "keep-alive" pings and monitoring consumption requires
-  // issuing authenticated requests to Colab. This can only be done after the
-  // user has signed in. We don't block extension activation on completing the
-  // heavily asynchronous sign-in flow.
-  const whileAuthorizedToggle = authProvider.whileAuthorized(
-    keepServersAlive,
-    consumptionMonitor.toggle,
-  );
 
   context.subscriptions.push(
-    uriHandler,
-    uriHandlerRegistration,
     disposeAll(authFlows),
     authProvider,
     assignmentManager,
     workbenchServerProvider,
-    keepServersAlive,
-    ...consumptionMonitor.disposables,
-    whileAuthorizedToggle,
   );
-}
-
-/**
- * Sets up consumption monitoring.
- *
- * If the user has already signed in, starts immediately. Otherwise, waits until
- * the user signs in.
- */
-function watchConsumption(colab: ColabClient): {
-  toggle: Toggleable;
-  disposables: Disposable[];
-} {
-  const disposables: Disposable[] = [];
-  const poller = new ConsumptionPoller(vscode, colab);
-  disposables.push(poller);
-  const notifier = new ConsumptionNotifier(
-    vscode,
-    colab,
-    poller.onDidChangeCcuInfo,
-  );
-  disposables.push(notifier);
-
-  return { toggle: poller, disposables };
 }
 
 
