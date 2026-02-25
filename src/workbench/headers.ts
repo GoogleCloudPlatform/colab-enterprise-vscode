efbb97357073448d04e0dc1f96d1c9fd67839b2e
Ola Mysiak <amysiak@google.com>
2026-02-25 15:19:08 +0100

    Change header to the standard X-Goog-Api-Client

--- src/workbench/headers.ts
+++ src/workbench/headers.ts
@@ -29,8 +29,8 @@ n * The HTTP header for the Workbench client agent used for requests originating
  * from VS Code.
  */
 export const WORKBENCH_CLIENT_AGENT_HEADER: StaticHeader = {
-  key: 'X-Workbench-Client-Agent',
-  value: 'vscode',
+  key: 'X-Goog-Api-Client',
+  value: 'workbench-vscode-ext/1.0.0',
 };
 
 /**
