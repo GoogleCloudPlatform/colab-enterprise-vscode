3184be3b3f8ca413bb54ff189363f240446f92fa
Andrii Lobanov <anloba@google.com>
2026-04-13 08:52:32 +0000

    logger commit
    Change-Id: I8d330f7b42d8b98f9c377c47f924bccdb101c9ec

--- src/common/logging/logger.ts
+++ src/common/logging/logger.ts
@@ -197,6 +197,5 @@ function getConfiguredLogLevel(vs: typeof vscode): LogLevel {
     .getConfiguration('workbench.logging')
     .get<Lowercase<keyof typeof LogLevel>>('level', 'info');
 
-  const level = LOG_CONFIG_TO_LEVEL[configLevel] as LogLevel | undefined;
-  return level ?? LogLevel.Info;
+  return LOG_CONFIG_TO_LEVEL[configLevel] ?? LogLevel.Info;
 }
